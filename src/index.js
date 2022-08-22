4535b06c7bb056f5f222957f37afd525d496f060
AlanFNL <89108362+AlanFNL@users.noreply.github.com>
2022-08-22 20:49:10 -0300

    Cambio MetaTags

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { hydrate, render } from "react-dom";
+ 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
 );
+
+
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
