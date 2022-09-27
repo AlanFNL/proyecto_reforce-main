469ec0109f3b128a978e8cd07e487286b1958a21
AlanFNL <89108362+AlanFNL@users.noreply.github.com>
2022-09-27 16:20:52 -0300

    aaaadad

--- src/Components/OurVerticals/OurVerticals.js
+++ src/Components/OurVerticals/OurVerticals.js
@@ -45,65 +45,7 @@ const OurVerticals = () => {
         <div ref={positionRef} className="class_our_verticals_container">
             <span ref={ref} className={"class_title" + ((inView) ? " class_animation_effect_slide" : "")}>{t("verticalsTitle")}</span>
 
-            <div ref={refTextDescription} className={"class_our_verticals_icons_container" + ((!inViewTextDescription) ? " class_our_verticals_icons_container_effect" : "")}>
-
-                <div className="class_our_verticals_box_container">
-
-                    {/* <OurVerticalIcons 
-                        icon={<Joystick/>} 
-                        title="PLAY TO EARN"
-                        description="
-                        Generación de tokens crypto, mediante la inversión en equipos y jugadores dentro de metaversos"
-                        type="left"
-                    /> */}
-
-                    {/* <OurVerticalIcons 
-                        icon={<Note/>} 
-                        title="LAUNCHPAD"
-                        description="
-                        Participacion en ofertas iniciales de tokens privadas, donde podemos adquirir divisas de tokens muy por debajo del precio del mercado, antes de que se publiquen los proyectos."
-                        type="left"
-                    /> */}
-
-                    <OurVerticalIcons 
-                        icon={<Rocket/>} 
-                        title={t("verticalMetaverseDevelopment")}
-                        items={[
-                            "verticalMetaverseCampaign",
-                            "verticalImmesirveExperiences",
-                            "vertical3DDesign&WorldGeneration",
-                            "verticalRoadmapDevelopment",
-                            "verticalContentGeneration"
-                        ]}
-                        type="left"
-                    />
-                </div>
-
-                <img src={ImageInfinite} className="class_our_verticals_image"/>
-
-                <div className="class_our_verticals_box_container">
-                    <OurVerticalIcons 
-                        icon={<Note/>} 
-                        title={t("verticalNFTFactory")}
-                        items={[
-                            "verticalWebAgencyConsulting",
-                            "verticalNFTDesign",
-                            "verticalCommunityBuilding",
-                            "verticalMarketPlaces",
-                            "verticalCreditCard"
-                        ]}
-                        type="right"
-                    />
-
-                    {/* <OurVerticalIcons 
-                        icon={<Rocket/>} 
-                        title="NFT FACTORY"
-                        description="
-                        Enfocada en el desarrollo de artículos únicos (NFT) con utilidades y valor agregado, y también a la generación de experiencias o tiendas en el metaverso."
-                        type="right"
-                    /> */}
-                </div>
-            </div>
+            
         </div>
     )
 }
